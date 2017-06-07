01529bd86173a74d96831e67172de402099e329a
casa <wangshentianfeng@163.com>
2017-06-07 14:53:24 +0800

    updated tooobar action and theme

--- app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
+++ app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
@@ -3,6 +3,9 @@ package guoer.lf.ed.guoer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class GuoerActivity extends AppCompatActivity {
 
@@ -12,5 +15,29 @@ public class GuoerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guoer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete:
+                Toast.makeText(this, "Delete action selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.add:
+                Toast.makeText(this, "Add action selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings action selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
