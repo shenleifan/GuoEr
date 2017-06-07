8cba1c621fcc0c0782edde982e5e62f136c55e97
casa <wangshentianfeng@163.com>
2017-06-08 05:45:32 +0800

    updated layout

--- app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
+++ app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
@@ -1,10 +1,12 @@
 package guoer.lf.ed.guoer;
 
 import android.annotation.TargetApi;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -16,15 +18,21 @@ import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import guoer.lf.ed.guoer.logUtils.LogUtils;
 
-public class GuoerActivity extends AppCompatActivity {
-
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
+public class GuoerActivity extends SimpleBaseActivity implements ToolBarFragment.OnFragmentInteractionListener{
+    private static final String TAG = "GuoerActivity";
+//    @BindView(R.id.toolbar)
+//    Toolbar mToolbar;
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
     @BindView(R.id.navigationView)
     NavigationView mNavigationView;
+
+    @Override
+    protected Fragment createFragment() {
+        return ToolBarFragment.newInstance(null, null);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +44,13 @@ public class GuoerActivity extends AppCompatActivity {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void init() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(getResources().getDrawable(android.R.drawable.ic_dialog_dialer, null));
-        }
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(getResources().getDrawable(android.R.drawable.ic_dialog_dialer, null));
+//        }
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -50,6 +58,7 @@ public class GuoerActivity extends AppCompatActivity {
                 return true;
             }
         });
+
 
     }
 
@@ -78,5 +87,10 @@ public class GuoerActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        LogUtils.d(TAG, "onFragmentInteraction");
     }
 }
---
+++ app/src/main/java/guoer/lf/ed/guoer/SimpleBaseActivity.java
@@ -0,0 +1,23 @@
+package guoer.lf.ed.guoer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SimpleBaseActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frame_container);
+        if (fragment == null) {
+            fragment = createFragment();
+        }
+        fragmentManager.beginTransaction().add(R.id.frame_container, fragment).commit();
+    }
+
+}
