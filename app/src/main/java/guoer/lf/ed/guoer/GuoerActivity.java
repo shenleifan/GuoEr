ea0e7eb302ed6524bb0915156fe74caee46ac0ec
casa <wangshentianfeng@163.com>
2017-06-07 14:38:43 +0800

    Added toolbar and menu

--- app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
+++ app/src/main/java/guoer/lf/ed/guoer/GuoerActivity.java
@@ -2,6 +2,7 @@ package guoer.lf.ed.guoer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class GuoerActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class GuoerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guoer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
